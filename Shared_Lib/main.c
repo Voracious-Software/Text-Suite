b8d03451a2fb562cc19d90a4b3c908eb79c8aab4
Axmill <anchorfall551@gmail.com>
2015-02-21 23:02:29 -0500

    Remove useless C library
    It had no purpose (I honestly have no idea why I added it).

--- Shared_Lib/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-int test(void)
-{
-    printf("Yep, it worked.");
-    return 0;
-}
--- Shared_Lib/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int test(void);
-
-#endif // MAIN_H_INCLUDED
